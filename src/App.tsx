90285e0fd8b830c15d43868bf4b5e7d2222aa81d
Kavitha-Softsuave <kavithaganesansoftsuave@gmail.com>
2025-10-16 16:27:32 +0530

    Add Persist state

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 import Index from "./pages/Index";
 import WidgetManagement from "./pages/WidgetManagement";
 import DashboardBuilder from "./pages/DashboardBuilder";
 import NotFound from "./pages/NotFound";
 import UploadPage from "./pages/UploadPage";
+import { PersistGate } from "redux-persist/integration/react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
@@ -25,12 +27,12 @@ const App = () => (
             <Route path="/widgets" element={<WidgetManagement />} />
             <Route path="/dashboard" element={<DashboardBuilder />} />
             <Route path="/upload" element={<UploadPage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
+    </PersistGate>
   </Provider>
 );
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
-import widgetReducer from './widgetSlice';
-import dashboardReducer from './dashboardSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import widgetReducer from "./widgetSlice";
+import dashboardReducer from "./dashboardSlice";
+
+const rootReducer = combineReducers({
+  widgets: widgetReducer,
+  dashboards: dashboardReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    widgets: widgetReducer,
-    dashboards: dashboardReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // because redux-persist stores non-serializable values
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
